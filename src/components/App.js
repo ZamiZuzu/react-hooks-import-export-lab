043721a5965fba3cf60f25e1a6fb8e0881618e7b
Christopher Valle <chrisbv@live.com>
2021-11-02 09:24:02 -0500

    commit tree 43961734a0a6914b7f0a409fadd753d79b054913

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+// import username from "./user"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username="Liza" city="New York"/>
+      <About image="https://i.imgur.com/mV8PQxj.gif"/>
     </div>
   );
 }
